e20aa3643eb5dcca6d510b6c7ecb0ed2eb79fc8b
井上 <173559396+INOUE-DevGit@users.noreply.github.com>
2024-12-17 23:57:03 +0900

    Rename StartVpnQuickControl.bat to StartVPNQuickControl.bat

---
+++ src/StartVPNQuickControl.bat
@@ -0,0 +1,29 @@
+@echo off
+
+rem _/_/_/_/_/_/_/_/_/ ユーザー設定 _/_/_/_/_/_/_/_/_/
+
+rem アプリケーションを配置しているディレクトリのパス
+set APP_DIR="C:\Dev\VPNQuickControl"
+
+rem アプリケーションの実行ファイル名
+set APP_EXE="VPNQuickControl.exe"
+
+rem _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+
+if not exist %APP_DIR% (
+    echo 必要なディレクトリが存在しません: %APP_DIR%
+    pause
+    exit
+)
+if not exist %APP_DIR%\%APP_EXE% (
+    echo 必要な実行ファイルが存在しません: %APP_DIR%\%APP_EXE%
+    pause
+    exit
+)
+
+cd %APP_DIR%
+start "" %APP_EXE%
+
+exit
+
